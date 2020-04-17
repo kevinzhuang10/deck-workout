00df3e78f6f1760a8545ce10b3ee594a1684aeac
Kevin Huang <kevinzhuang10@gmail.com>
2020-04-17 10:11:52 -0700

    error alerts

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-// import { createMessage, returnErrors } from './messages';
+import { createMessage, returnErrors } from "./messages";
 // import { tokenConfig } from "./auth";
 
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
@@ -15,8 +15,9 @@ export const getLeads = () => (dispatch, getState) => {
 			});
 		})
 		.catch((err) => {
-			// return dispatch(returnErrors(err.response.data, err.response.status))
-			console.log(err);
+			return dispatch(
+				returnErrors(err.response.data, err.response.status)
+			);
 		});
 };
 
@@ -25,6 +26,7 @@ export const deleteLead = (id) => (dispatch, getState) => {
 	axios
 		.delete(`/api/leads/${id}/`)
 		.then((res) => {
+			dispatch(createMessage({ deleteLead: 'Lead Deleted' }));
 			dispatch({
 				type: DELETE_LEAD,
 				payload: id,
@@ -38,14 +40,13 @@ export const addLead = (lead) => (dispatch, getState) => {
 	axios
 		.post("/api/leads/", lead)
 		.then((res) => {
-			// dispatch(createMessage({ addLead: "Lead Added" }));
+			dispatch(createMessage({ addLead: "Lead Added" }));
 			dispatch({
 				type: ADD_LEAD,
 				payload: res.data,
 			});
 		})
-		// .catch((err) =>
-		// 	dispatch(returnErrors(err.response.data, err.response.status))
-		.catch((err) => console.log(err));
-	// );
+		.catch((err) =>
+			dispatch(returnErrors(err.response.data, err.response.status))
+		);
 };
